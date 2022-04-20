619a5de701100a2a6b65d17d551afeb63052025d
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-04-20 12:03:01 +0530

    corrected a typo in the project name

--- backend/edicationalRepository/educationalRepository/wsgi.py
+++ backend/edicationalRepository/educationalRepository/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for edicationalRepository project.
+WSGI config for educationalRepository project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edicationalRepository.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educationalRepository.settings')
 
 application = get_wsgi_application()

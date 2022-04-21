1c6db33cb849464219c414146f356cafe037dc07
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-04-21 14:52:02 +0530

    Added the CRUD Methods for the user collection

--- backend/educationalRepository/repositoryAPI/views.py
+++ backend/educationalRepository/repositoryAPI/views.py
@@ -15,11 +15,25 @@ from rest_framework.parsers import JSONParser
 
 class UserView(views.APIView):
     def get(self, request):
-        cursor = findAllDocument("test_db", "users_col", {'user_points': 100})
+        cursor = findAllDocument("test_db", "users_col", {})
         user_data = []
         for doc in cursor:
             user_data.append(doc)
         result = UserSerializer(user_data, many=True).data
-        return Response(result)
+        return JsonResponse(result, safe=False)
         # return JsonResponse(result)
+    
+    def post(self, request):
+        data = JSONParser().parse(request)
+        result = saveSingleDocument("test_db", "users_col", data)
+        return JsonResponse(result)
         
+    def put(self, request):
+        data = JSONParser().parse(request)
+        result = saveSingleDocument("test_db", "users_col", data)
+        return JsonResponse(result)
+    
+    def delete(self, request):
+        data = JSONParser().parse(request)
+        result = deleteDocument("test_db", "users_col", data)
+        return JsonResponse(result)

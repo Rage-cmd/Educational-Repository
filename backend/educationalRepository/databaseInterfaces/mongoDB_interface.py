100a749dee441db43a35c5528235d3406fb7c998
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-04-21 13:33:06 +0530

    rest framework testing

--- backend/educationalRepository/databaseInterfaces/mongoDB_interface.py
+++ backend/educationalRepository/databaseInterfaces/mongoDB_interface.py
@@ -1,7 +1,7 @@
+from cgi import test
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient()
 
 def getCollection(Database, collection):
     db = client[Database]
@@ -15,8 +15,8 @@ def saveSingleDocument(Database, collection, object):
     objectId = col.insert_one(object).inserted_id
     return objectId
 
-def saveMultipleDocuments(Database, collection, objects):
-    col = getCollection(Database,collection)
+def saveMultipleDocuments(Database, collections, objects):
+    col = getCollection(Database,collections)
     objectIds = col.insert_many(objects).inserted_ids
     return objectIds
 
@@ -32,3 +32,61 @@ def findAllDocument(Database, collection, filterObject):
 
 # saveDocument("test_db","test_col",{"name":"asda", "age":10})
 # print(findAllDocument("test_db", "test_col", {"name":"asda"}))
+
+
+# create a mongodb database
+
+# test_db = client["test_db"]
+# users_col = test_db["users"]
+
+# users = [
+#     {
+#         'user_name': 'John Doe',
+#         'user_email': 'user@example.com',
+#         'user_password': 'password',
+#         'user_access_level': 'user',
+#         'user_status': 'active',
+#         'user_posts': ['p12', 'p52'],
+#         'user_comments': ['c13', 'c66'],
+#         'user_points': 100,
+#         'user_profile_picture': 'https://example.com/profile.png',
+#         'user_no_of_bans': 0,
+#         'user_is_banned': False
+#     },
+#     {
+#         'user_name': 'Jane Doe',
+#         'user_email': 'user2@example.com',
+#         'user_password': 'password',
+#         'user_access_level': 'user',
+#         'user_status': 'active',
+#         'user_posts': ['p17', 'p32'],
+#         'user_comments': ['c53', 'c67'],
+#         'user_points': 100,
+#         'user_profile_picture': 'https://example.com/profile2.png',
+#         'user_no_of_bans': 0,
+#         'user_is_banned': False
+#     },
+#     {
+#         'user_name': 'Nathan Smith',
+#         'user_email': 'user3@example.com',
+#         'user_password': 'password',
+#         'user_access_level': 'moderator',
+#         'user_status': 'inactive',
+#         'user_posts': ['p18', 'p33'],
+#         'user_comments': ['c54', 'c68'],
+#         'user_points': 114,
+#         'user_profile_picture': 'https://example.com/profile3.png',
+#         'user_no_of_bans': 0,
+#         'user_is_banned': False
+#     }
+# ]
+
+# saveMultipleDocuments("test_db", "users_col", users)
+
+# if __name__ == "__main__":
+client = MongoClient()
+
+users = findAllDocument("test_db", "users_col", {})
+
+for doc in users:
+    print(doc)
--- backend/educationalRepository/repositoryAPI/models.py
+++ backend/educationalRepository/repositoryAPI/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+# class User(models.Model):
+#     user_id = models.AutoField(primary_key=True)
+#     user_name = models.CharField(max_length=100)
+#     user_email = models.EmailField(max_length=100)
+#     user_password = models.CharField(max_length=100)
+#     user_access_level = models.CharField(max_length=100)
+#     user_status = models.CharField(max_length=100)
+#     user_posts = ArrayField(models.CharField(max_length=100))
+#     user_comments = ArrayField(models.CharField(max_length=100))
+#     user_points = models.IntegerField()
+#     user_profile_picture = models.CharField(max_length=100)
+#     user_no_of_bans = models.IntegerField()
+#     user_is_banned = models.BooleanField()
+    

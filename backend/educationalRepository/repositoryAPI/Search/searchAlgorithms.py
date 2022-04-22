ef49d560d5936d30624acf913bee73b0f4b6beb0
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-04-22 15:51:48 +0530

    Added the search by tag ID

--- backend/educationalRepository/repositoryAPI/Search/searchAlgorithms.py
+++ backend/educationalRepository/repositoryAPI/Search/searchAlgorithms.py
@@ -15,6 +15,20 @@ def post_name_search(post_name):
     return result
 
 def tag_ID_search(tag_id):
-    tag = mongoDB_interface.findSingleDocument("test_db","tags_collection",{"id":tag_id})
-    path = tag['path_to_tag']
+    main_tree_node = mongoDB_interface.findSingleDocument("test_db","maintree_collection",{"id":tag_id})
+    post_nodes = []
+    queue = [main_tree_node]
+    while queue:
+        node = queue.pop(0)
+        if node['type'] == 'post':
+            post_nodes.append(node)
+        for child in node['children_tags']:
+            queue.append(mongoDB_interface.findSingleDocument("test_db","maintree_collection",{"id":child}))
+        for child in node['children_posts']:
+            queue.append(mongoDB_interface.findSingleDocument("test_db","maintree_collection",{"id":child}))
+    
+    result = []
+    for post in post_nodes:
+        result.append(mongoDB_interface.findSingleDocument("test_db","posts_collection",{"id":post['id']}))
+    return result
     
--- backend/educationalRepository/repositoryAPI/tests.py
+++ backend/educationalRepository/repositoryAPI/tests.py
@@ -6,6 +6,10 @@ from Search.searchAlgorithms import *
 # post = post_ID_search('p12')
 # print(post)
 
-post_caption = post_name_search('caption')
+# post_caption = post_name_search('caption')
+# print(post_caption)
 
-print(post_caption)
+posts = tag_ID_search('t5')
+
+for post in posts:
+    print(post['caption'])

3ab246d5d9228eba80949d62a0329a0f40bdb0ee
bruise_88 <sidshah1036.ss@gmail.com>
2022-04-24 14:22:35 +0530

    added Home Screen Template

--- frontend/client-app/src/App.vue
+++ frontend/client-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
+
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,42 +36,28 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
-      <PostTemplate :verifiedPost="true" :postModel="sampleMCQPostData"/>
-      <PostTemplate :verifiedPost="true" :postModel="sampleVideoPostData"/>
+
+      <HomeScreen />
     </v-main>
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import PostTemplate from './components/PostTemplate.vue';
+import HomeScreen from './components/HomeScreen.vue';
 
 export default {
   name: 'App',
 
   components: {
-    PostTemplate
+    HomeScreen,
 },
 
   data: () => ({
-    sampleVideoPostData:{
-      "title":"Thor Trailer",
-      "verifiedPost":true,
-      "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos recusandae nesciunt ad voluptate aperiam exercitationem veritatis laudantium totam a, natus inventore, sunt unde laborum nam obcaecati neque. Velit, obcaecati quas. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam temporibus optio deserunt, at odit dolorem nostrum magnam dolores sequi delectus, corporis reiciendis. Provident tempora minus maiores iure, cum fugit quasi.",
-      "videoURL":"https://www.youtube.com/embed/tgB1wUcmbbw",
-      "postType":"video",
-    },
-    sampleMCQPostData:{
-      "verifiedPost":true,
-      "title":"Find the value of x",
-      "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos recusandae nesciunt ad voluptate aperiam exercitationem veritatis laudantium totam a, natus inventore, sunt unde laborum nam obcaecati neque. Velit, obcaecati quas. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam temporibus optio deserunt, at odit dolorem nostrum magnam dolores sequi delectus, corporis reiciendis. Provident tempora minus maiores iure, cum fugit quasi.",
-      "imgURL":"https://hi-static.z-dn.net/files/d62/0c3678b43230a26c13d6f16947d28a03.jpg",
-      "postType":"MCQ",
-      "Options":""
-    }
+    
   }),
 };
 </script>
---
+++ frontend/client-app/src/components/NavBar/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+    
+    <v-container >
+            <v-app-bar
+            color="primary"
+            dark
+            elevate-on-scroll
+            absolute
+            scroll-target="#scrolling-techniques-7"
+            >
+                <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+
+            <!-- <v-row ml-4> -->
+                   <v-btn      
+                   v-for="opt in navBarOptions" :key="opt"
+                   color="primary"
+                   flat
+                   elevation="0"
+                    >
+                        {{opt}}
+                    </v-btn>
+            <!-- </v-row> -->
+                
+            <v-spacer></v-spacer>
+
+                <v-btn icon>
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                    <v-icon>mdi-logout</v-icon>
+                </v-btn>
+            </v-app-bar>
+
+            <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+            >
+                <v-list
+                    nav
+                >
+                    <v-list-item-group
+                    v-model="group"
+                    active-class="deep-purple--text text--accent-4"
+                    >
+                    <v-list-item>
+                        <v-list-item-icon>
+                        <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-icon>
+                        <v-icon>mdi-account</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Account</v-list-item-title>
+                    </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-navigation-drawer>
+    </v-container>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    data:()=>({
+        userDetails:{
+            "username":"Siddharth",
+        },
+        drawer: false,
+        group: null,
+        navBarOptions:["Home","Notifications","Your Uploads","Watchlist"]
+    })
+}
+</script>

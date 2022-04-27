b5de111a065a89fa7f0c990be3df4e42730bd82d
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-04-27 14:03:43 +0530

    Added initial admin utilities

---
+++ backend/educationalRepository/repositoryAPI/Admin/adminUtilities.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("..")
+
+import databaseInterfaces.mongoDB_interface as mongoDB_interface
+
+def assign_role(user_id, role="user"):
+    """
+    Assigns a role to a user.
+
+    Parameters:
+        user_id (str): The user's id.
+        role (str): The role to assign. Default is "user".
+
+    """
+    try:
+        user = mongoDB_interface.findSingleDocument("test_db","users_collection",{"id":user_id})
+        user["access_level"] = role
+        mongoDB_interface.updateDocument("test_db","users_collection",{"id":user_id},{"$set":{'access_level':role}})
+    except:
+        print("Cannot assign role. User not found.")
+
+
+def ban_user(user_id):
+    """
+    Bans a user.
+
+    Parameters:
+        user_id (str): The user's id.   
+    """
+
+    try:
+        user = mongoDB_interface.findSingleDocument("test_db","users_collection",{"id":user_id})
+        user["is_banned"] = True
+        mongoDB_interface.updateDocument("test_db","users_collection",{"id":user_id},{"$set":{'is_banned':True}})
+    except:
+        print("Cannot ban user. User not found.")

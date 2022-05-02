24186c7c82453dcbc651b98f59f4682a5e876097
bruise_88 <sidshah1036.ss@gmail.com>
2022-05-03 00:33:12 +0530

    added upload video post screen

--- frontend/client-app/src/App.vue
+++ frontend/client-app/src/App.vue
@@ -1,54 +1,20 @@
 <template>
 
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
 
     <v-main>
       <NavBar :sideMenu="sideMenuMap[user.user_level]" @sideMenuSelect="sideMenuSelectHandler"/>
-
-      <HomeScreen v-if="currentScreen=='Home'" :currentScreen="currentScreen" />
+      <v-container >
+        
+      </v-container>
+      <HomeScreen v-if="currentScreen=='Home' && !hideall" :currentScreen="currentScreen" />
       <!-- <PendingApprovalsScreen v-if="currentScreen == 'Pending Approvals'" :currentScreen="currentScreen"/>
        -->
 
-      <PostsList v-else :currentScreen="currentScreen"/>
+      <PostsList v-if="currentScreen=='Watchlist' && !hideall" :currentScreen="currentScreen"/>
 
       <v-btn
-                v-show="!hidden"
+                v-show="!hidden && !hideall"
                 color="pink"
                 dark
                 fixed
@@ -58,6 +24,8 @@
               >
                 <v-icon>mdi-plus</v-icon>
       </v-btn>
+
+      <UploadScreen />
       
 
     </v-main>
@@ -68,14 +36,16 @@
 // import HelloWorld from './components/HelloWorld';
 import HomeScreen from './components/HomeScreen.vue';
 // import PendingApprovalsScreen from './components/Moderator/PendingApprovalsScreen.vue'
-import NavBar from './components/NavBar/NavBar.vue'
-import PostsList from './components/PostsList.vue'
+import NavBar from './components/NavBar/NavBar.vue';
+import PostsList from './components/PostsList.vue';
+import UploadScreen from './components/CreationScreens/UploadScreen.vue';
 
 export default {
   name: 'App',
 
   components: {
     HomeScreen,
+    UploadScreen,
     // PendingApprovalsScreen,
     NavBar,
     PostsList,
@@ -90,7 +60,8 @@ export default {
     sideMenuMap:{
       "Moderator":["Your Uploads","My Profile", "Watchlist","Pending Approvals","Reported Users","Banned Users"],
       "user":["Your Uploads","My Profile", "Watchlist"],
-    }
+    },
+    hideall: true,
   }),
   methods:{
     sideMenuSelectHandler(opt){
---
+++ frontend/client-app/src/components/CreationScreens/UploadScreen.vue
@@ -0,0 +1,45 @@
+<template>
+<v-container mt-6 class="d-flex justify-center">
+        
+    <v-card width="70%" mt-2 elevation="8">
+        <v-card-text>
+            <v-form width="50%">
+                <v-text-field
+                    width="20%"
+                    label="Title"
+                ></v-text-field>
+
+                <v-file-input
+                    accept="image/*"
+                    label="Video File"
+                ></v-file-input>
+
+                <v-textarea
+                    outlined
+                    name="input-7-4"
+                    label="Description"
+                ></v-textarea>
+
+                <v-autocomplete 
+                        chips
+                        small-chips
+                        dense
+                        multiple
+                        :items="suggestionOptions"
+                        label="Tags"
+                        ></v-autocomplete>
+            
+            </v-form>
+            <v-card-actions class="d-flex justify-center">
+            <v-btn
+                color="primary"
+                @click="reveal = true"
+            >
+                Post
+            </v-btn>
+            </v-card-actions>
+            
+        </v-card-text>
+    </v-card>
+</v-container>
+</template>

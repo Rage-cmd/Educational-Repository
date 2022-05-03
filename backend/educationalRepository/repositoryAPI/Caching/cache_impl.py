a6b87b0d81c3c57fd561d661e3a0d34c13e883fd
bruise_88 <sidshah1036.ss@gmail.com>
2022-05-03 11:56:53 +0530

    implemented caching

---
+++ backend/educationalRepository/repositoryAPI/Caching/cache_impl.py
@@ -0,0 +1,52 @@
+from queue import PriorityQueue
+# from datetime import datetime
+
+class CacheImpl:
+    
+    
+    def __init__(self, maxSize=1):
+        self.most_recent_cache = PriorityQueue()
+        self.most_upvoted_cache = PriorityQueue()
+        self.maxItems = maxSize
+    
+    def addItem_recent_cache(self,id, datetime):
+        insertItem = (datetime,id)
+        if len(self.most_recent_cache.queue) >= self.maxItems:
+            removedItem = (self.most_recent_cache).get()
+            insertItem = max(removedItem,insertItem)
+        (self.most_recent_cache).put(insertItem)
+
+    
+    def getAllItems_recent_cache(self):
+        postIds = []
+        for i in range(len(self.most_recent_cache.queue)):
+            postIds.append(self.most_recent_cache.queue[i][1])
+        return postIds
+
+    def addItem_upvote_cache(self,id, upvotes):
+        insertItem = (-upvotes,id)
+        if len(self.most_upvoted_cache.queue) >= self.maxItems:
+            removedItem = (self.most_upvoted_cache).get()
+            insertItem = min(removedItem, insertItem)
+        (self.most_upvoted_cache).put(insertItem)
+
+    def getAllItems_upvote_cache(self):
+        postIds=[]
+        for i in range(len(self.most_upvoted_cache.queue)):
+            postIds.append(self.most_upvoted_cache.queue[i][1])
+        return postIds
+
+
+# cache = CacheImpl(2)
+
+# cache.addItem_recent_cache(1,datetime.now())
+# cache.addItem_recent_cache(2,datetime(2019,1,1,0,0,0))
+# cache.addItem_recent_cache(3,datetime(2020,1,1,0,0,0))
+
+# cache.addItem_upvote_cache(1,4)
+# cache.addItem_upvote_cache(3,10)
+# cache.addItem_upvote_cache(2,11)
+
+# print(cache.getAllItems_recent_cache())
+# print(cache.getAllItems_upvote_cache())
+

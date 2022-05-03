ab08f60ed420910b7f9a69ca07564078528c510b
bruise_88 <sidshah1036.ss@gmail.com>
2022-05-03 13:29:08 +0530

    added comment dialog

---
+++ frontend/client-app/src/components/Comment/CommentDialog.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-dialog
+        v-model="dialog"
+        scrollable
+        :overlay="true"
+        width="90%"
+        transition="dialog-transition"
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+                    <v-icon>mdi-comment-outline</v-icon>
+            </v-btn>
+        </template>
+        <v-card height="100%">
+            <v-row class="mb-0">
+                <v-col cols=6 mr-0>
+                    <PostView :postModel="postModel" :showActionTray="false"/>
+                </v-col>
+                <v-col cols=6 ml-0>
+                
+                    <v-list dense disabled height="100%">
+                    <v-subheader>Comments</v-subheader>
+                    <v-list-item-group
+                        color="primary"
+                    >
+                        <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+                        elevation="1"
+                        >
+                        <v-list-item-avatar>
+                            <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title v-html="item.commentedby"></v-list-item-title>
+                            <v-list-item-subtitle v-html="item.comment"></v-list-item-subtitle>
+                        </v-list-item-content>
+                        <!-- <v-list-item-content>
+                            <v-list-item-title v-text="item.text"></v-list-item-title>
+                        </v-list-item-content> -->
+                        </v-list-item>
+                    </v-list-item-group>
+                    </v-list>
+
+                </v-col>
+            </v-row>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import PostView from './PostView.vue';
+export default {
+    name:'CommentDialog',
+    components:{
+        PostView,
+    },
+    setup() {
+        
+    },
+    props:{
+        postModel:Object,
+    },
+    data:()=>({
+        items : [{"commentedby":"user1",
+                    "comment":"a + b = c"},
+                {"commentedby":"user2",
+                    "comment":"Albert Einstein"}]
+    }),
+}
+</script>
---
+++ frontend/client-app/src/components/Comment/PostView.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-card
+    :loading="loading"
+    elevation = "1"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+
+    <v-card-title>{{postModel.title}}</v-card-title>
+    
+    
+    <v-card-text secondary-title>
+        <v-icon small>mdi-account</v-icon>
+        {{postModel.username}}
+    </v-card-text>
+
+
+    <iframe v-if="postModel.postType === 'video'" width="100%" height="400" :src="postModel.videoURL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    </iframe>
+
+    <v-img v-if="postModel.postType === 'MCQ'" :src="postModel.imgURL" width="100%"></v-img>
+
+
+    <v-card-text>
+
+      <div class="my-4 text-subtitle-1 font-weight-bold">
+        Description
+      </div>
+
+      <div>
+          {{description}}
+          <!-- <v-btn text @click="fullDescription=true">read more..</v-btn> -->
+          <a href="javascript:void(0)" @click="toggleDescription(postModel)" v-if="!fullDescription"> read more...</a>
+          <!-- <a href="javascript:void(0)" @click="toggleDescription(postModel)"> </a> -->
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-text>
+      <v-chip-group
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-chip>tag1</v-chip>
+
+        <v-chip>tag2</v-chip>
+
+        <v-chip>tag3</v-chip>
+
+        <v-chip>tag4</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+
+
+
+<script>
+
+
+export default {
+  name: 'PostTemplate',
+  data(){
+      return{
+        fullDescription:false,
+        description:"",
+        loading:false,
+      }
+  },
+  props:{
+      postModel:Object,
+  },
+
+  methods:{
+      getDescription(postModel){
+          if(this.fullDescription == true){
+              return postModel.description;
+          }
+          else{
+              return postModel.description.substring(0,250)
+          }
+      },
+
+      toggleDescription(postModel){
+            this.fullDescription = !this.fullDescription;
+            this.description = this.getDescription(postModel);
+      },
+  },
+  created: function(){
+      this.description = this.getDescription(this.postModel);
+  },
+
+}
+</script>

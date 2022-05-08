4e0f7cf63237b6396621b221a11c78c1c26fdce9
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-05-08 14:56:20 +0530

    Update urls.py

--- backend/educationalRepository/repositoryAPI/urls.py
+++ backend/educationalRepository/repositoryAPI/urls.py
@@ -2,7 +2,11 @@ from django.urls import include, path
 from repositoryAPI import views
 
 urlpatterns = [
+    # path('user', views.GenericView.as_view(collection='users_collection')),
+    # path('post', views.GenericView.as_view(collection='posts_collection')),
+    # path('comment', views.GenericView.as_view(collection='comments_collection')),
     path('user', views.GenericView.as_view(collection='users_collection')),
     path('post', views.GenericView.as_view(collection='posts_collection')),
     path('comment', views.GenericView.as_view(collection='comments_collection')),
+    path('login', views.login_creds),
 ]

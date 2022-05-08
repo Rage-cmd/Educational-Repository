75158a2dae0fbc9d67d67a30527d946ed0233b30
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-05-08 14:52:54 +0530

    Added temporary test script for integration

--- frontend/client-app/src/components/LoginVue.vue
+++ frontend/client-app/src/components/LoginVue.vue
@@ -16,12 +16,12 @@
                         <v-card class="ml-15 mt-15" width="500">
                         <v-card-title>Login</v-card-title>
                         <v-card-text>
-                            <v-text-field prepend-icon="mdi-account-circle" label="Username" clearable></v-text-field>
-                            <v-text-field prepend-icon="mdi-lock" label="Password" type="password"></v-text-field>
+                            <v-text-field v-model="username" prepend-icon="mdi-account-circle" label="Username" clearable></v-text-field>
+                            <v-text-field v-model="password" prepend-icon="mdi-lock" label="Password" type="password"></v-text-field>
                         </v-card-text>
                         <v-divider></v-divider>
                         <v-card-actions>
-                            <v-btn color="success">Login</v-btn>
+                            <v-btn @click="submitForm" color="success">Login</v-btn>
                             <v-btn color="info">Sign Up</v-btn>
                         </v-card-actions>
                     </v-card>
@@ -42,8 +42,29 @@ export default ({
         
     },
     data: ()=>({
-        
+        username: "",
+        password: "",
     }),
+    methods: {
+        async submitForm(){
+            const response = await this.$http.post('http://127.0.0.1:8000/api/login', {
+                username: this.username,
+                password: this.password,
+            });
+
+            console.log(response.body);
+
+            this.username= "";
+            this.password= "";
+            
+            if(response.status == 200){
+                this.$store.commit('setUser', response.body);
+                this.$store.commit('setCurrentScreen', 'Home');
+            }
+
+        
+        }
+    }
 
 })
 </script>

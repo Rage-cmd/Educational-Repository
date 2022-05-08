01c549f809df12ff35d658d37b64790a9dff0473
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-05-08 14:54:59 +0530

    Added the upload endpoint

--- backend/educationalRepository/repositoryAPI/views.py
+++ backend/educationalRepository/repositoryAPI/views.py
@@ -124,3 +124,52 @@ def sign_up(request):
         return JsonResponse(user_document, safe=False, status=200)
     else:
         return HttpResponse("Invalid request", status=400)
+
+
+def upload_user_post(request):
+    """
+    Upload a post. The function expects a user id and a dictionary consisiting of post details.
+
+    The dictionary should contain the following keys:
+        type (str) : The type of the post.
+        text (str) : The text of the post.
+        caption (str) : The caption of the post.
+        tags (list) : A list of tag IDs. The tag IDs should be in the order of tree heirarchy i.e [root, first child, second child, ...].
+
+    Optional keys:
+        image_url (str) : The url of the image.
+        video_url (str) : The url of the video.
+        new_tag (str) : The name of the new tag. The new tag will be created if it does not exist. Use this key only if the tag is not in the tags list. 
+    
+    Just using the tags list will create a post with the tags present in the list (the order of tags in the list is important).
+    Using tags with the new_tags field will create a post under the tags list with the new tag.
+
+    The function returns a boolean value indicating if the post was uploaded successfully.
+
+    Hence, the HTTP Post request should be of the following format: 
+    Example::
+
+        data ={
+            "user_id" : <user_id>,
+            "post_details" : {
+                "type" : <post_type>,
+                "text" : <post_text>,
+                "caption" : <post_caption>,
+                "tags" : [<tag_id_1>, <tag_id_2>, ...], // The order "must" be [root, first child, second child, ...]
+                "image_url" : <image_url>,  // OPTIONAL
+                "video_url" : <video_url>,  // OPTIONAL
+                "new_tag" : <new_tag_name>  // OPTIONAL
+            }
+        }
+    """
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user_id = data['user_id']
+        post_details = data['post_details']
+        try:
+            upload_post(user_id, post_details,cache)
+            return HttpResponse("Post uploaded successfully.")
+        except Exception as e:
+            return HttpResponse("Post upload failed. The error is: " + str(e))
+    else:
+        return HttpResponse("Invalid request")

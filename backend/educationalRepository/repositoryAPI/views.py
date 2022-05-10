6feca012824840df1d7847e7c1f2752857592f12
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2022-05-10 23:06:12 +0530

    added comment details in search response

--- backend/educationalRepository/repositoryAPI/views.py
+++ backend/educationalRepository/repositoryAPI/views.py
@@ -514,11 +514,26 @@ def search(request):
         try:
             results = None
             if search_type == "post":
-                results = post_ID_search(search_term)
+                post = post_ID_search(search_term)
+                results = get_detailed_post(post['id'])
             # elif search_type == "post":
             #     results = post_name_search(search_term)
             elif search_type == "tag":
-                results = tag_ID_search(search_term)
+                posts = tag_ID_search(search_term)
+                results = []
+                for post in posts:
+                    comments = []
+                    for comment_id in post['comments']:
+                        comment = findSingleDocument("test_db","comments_collection",{"id":comment_id})
+                        author = findSingleDocument("test_db","users_collection",{"id":comment["author"]})
+                        comment['author'] = {
+                            "id" : author['id'],
+                            "name" : author['name'],
+                            "profile_picture" : author['profile_picture'],
+                        }
+                        comments.append(comment)
+                    post['comments'] = comments
+                    results.append(post)
         
             return JsonResponse(json.loads(json.dumps(results, default=str)), safe=False, status=200)
         except Exception as e:
